bb9cd4323d21448f4752fb6a03105c0f3449cf33
Dennis Fricke <dennis.fricke@dennis-fricke.de>
2024-12-19 11:01:51 +0100

    new dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,177 @@
-export { Notification, NotificationContext, useNotification } from './components';
-export type { DataTableProps, ColumnDefinition } from './components';
-export { DataTable } from './components';
-export { ObjectEditor, ObjectEditorRef, CheckboxSelection, FormSkeleton, Form, CheckboxSelectionOption, CheckboxSelectionProps } from './components';
-export type { EditorFieldType, EditorField, ChangeListener } from './components';
-export { FetchError } from './components';
-export type { Configuration, Icons } from './components';
-export { buildIconsFromIconSet } from './components';
-export { ConfigurationProvider, ConfigurationContext, useConfiguration } from './components';
-export { ApplicationToolbar, FlexClear } from './components';
-export { LinkButton } from './components';
-export type { LinkButtonProps } from './components';
-export type { UseQueryDefinition, UseQueryResult, MutationQueryResult, MutationQueryTrigger, LastQueryInfo, LazyUseQuery, UseQuery, MutationQuery, QueryTrigger } from './lib';
+import * as React from 'react';
+import React__default, { ReactNode, MutableRefObject } from 'react';
+import { ButtonProps } from 'primereact/button';
+import { IconProp, IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { Toast } from 'primereact/toast';
+
+interface LinkButtonProps extends ButtonProps {
+    severity?: 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'help' | 'contrast';
+    faIcon?: IconProp;
+    route: string;
+}
+declare function LinkButton({ faIcon, route, ...rest }: LinkButtonProps): React__default.JSX.Element;
+
+declare function Notification({ children }: {
+    children?: ReactNode | ReactNode[];
+}): React__default.JSX.Element;
+
+declare const NotificationContext: React__default.Context<Toast>;
+declare const useNotification: () => Toast;
+
+interface LazyUseQuery<TArgs, TResult> {
+    (): [QueryTrigger<TArgs, TResult>, UseQueryResult<any>, LastQueryInfo<TArgs>];
+}
+interface LastQueryInfo<TArgs> {
+    lastArg?: TArgs;
+}
+interface QueryTrigger<TArgs, TResult> {
+    (args?: TArgs): Promise<UseQueryResult<TResult>>;
+}
+interface UseQuery<TArgs, TResult> {
+    (args?: TArgs): UseQueryDefinition<TResult>;
+}
+interface UseQueryDefinition<TResult> {
+    data?: TResult;
+    error?: any;
+    isLoading: boolean;
+    isFetching: boolean;
+    refetch: (() => void);
+}
+interface UseQueryResult<TResult> {
+    data?: TResult;
+    isLoading: boolean;
+    isError: boolean;
+    error?: any;
+}
+interface MutationQuery<TArgs, TResult> {
+    (): readonly [MutationQueryTrigger<TArgs, TResult>, MutationQueryResult<TResult>];
+}
+interface MutationQueryTrigger<TArgs, TResult> {
+    (args: TArgs): Promise<MutationQueryResult<TResult>>;
+}
+interface MutationQueryResult<TResult> {
+    data?: TResult;
+    error?: any;
+}
+
+interface ColumnDefinition {
+    key: string;
+    translationKey: string;
+}
+interface DataTableProps<TType> {
+    queryAll: UseQuery<void, TType>;
+    deleteItem: MutationQuery<string, TType>;
+    columns: ColumnDefinition[];
+    openItemRoute: string | ((rowData: TType) => string);
+    deletePromptTranslationKey: string;
+    isDeletable?: (rowData: TType) => boolean;
+    isViewable?: (rowData: TType) => boolean;
+    deleteErrorTranslationKey: string;
+    deleteSuccessTranslationKey: string;
+    emptyListTranslationKey: string;
+    dataTest: string;
+}
+declare function DataTable(props: DataTableProps<any>): React__default.JSX.Element;
+
+interface CheckboxSelectionOption {
+    id: string;
+    name: string;
+}
+interface CheckboxSelectionProps {
+    onChange: (selectedValues: any[]) => void;
+    value: any[];
+    options: CheckboxSelectionOption[] | UseQuery<void, CheckboxSelectionOption[]>;
+}
+declare function CheckboxSelection(props: CheckboxSelectionProps): React__default.JSX.Element;
+
+interface ChangeListener {
+    (change: any): void;
+}
+type EditorFieldType = 'text' | 'editor' | 'number' | 'date' | 'datetime' | 'password' | 'email' | 'option' | 'option-multi' | 'selection-check' | 'selection-radio' | 'generated-secret' | undefined;
+interface EditorField {
+    key: string;
+    editable?: boolean;
+    labelTranslationKey: string;
+    placeholderTranslationKey?: string;
+    type: EditorFieldType;
+    size?: number;
+    allowedCharacters?: string;
+    options?: CheckboxSelectionOption[] | UseQuery<void, CheckboxSelectionOption[]>;
+}
+
+interface FormProps<TObject> {
+    object: TObject;
+    rows: EditorField[][];
+    onChange: (changedObject: any) => void;
+    onSave: () => void;
+}
+declare function Form(props: FormProps<any>): React__default.JSX.Element;
+
+interface FormSkeletonProps {
+    rows: EditorField[][];
+}
+declare function FormSkeleton(props: FormSkeletonProps): React__default.JSX.Element;
+
+interface ObjectEditorRef {
+    save(): void;
+}
+interface ObjectEditorProps<TObject> {
+    id: string;
+    fields: EditorField[];
+    fieldSizes?: number[];
+    useLazyGetObject: LazyUseQuery<any, TObject>;
+    useCreateObject: MutationQuery<TObject, TObject>;
+    useUpdateObject: MutationQuery<TObject, TObject>;
+    objectEditorRef: MutableRefObject<ObjectEditorRef | undefined>;
+    onSaved: (newObject: TObject) => void;
+    saveSuccessTranslationKey: string;
+    saveErrorTranslationKey: string;
+    additionalContent?: (object: TObject, onChange: (change: any) => void) => any | React__default.ReactElement | React__default.ReactElement[];
+}
+declare function ObjectEditor(props: ObjectEditorProps<any>): React__default.JSX.Element;
+
+interface SerializedError {
+    name?: string;
+    message?: string;
+    stack?: string;
+    code?: string;
+}
+declare function FetchError({ error }: {
+    error: SerializedError | any;
+}): React__default.JSX.Element;
+
+interface Icons {
+    faArrowLeft: IconDefinition;
+    faEye: IconDefinition;
+    faSearch: IconDefinition;
+    faTrash: IconDefinition;
+    faXmark: IconDefinition;
+    faRotateRight: IconDefinition;
+}
+interface Translations {
+    generate: string;
+}
+interface Configuration {
+    iconSet: Icons;
+    translations: Translations;
+}
+declare function buildIconsFromIconSet(iconSet: Icons): Icons;
+
+declare function ConfigurationProvider({ children, configuration }: {
+    children?: ReactNode | ReactNode[];
+    configuration: Configuration;
+}): React__default.JSX.Element;
+
+declare const ConfigurationContext: React.Context<Configuration>;
+declare const useConfiguration: () => Configuration;
+
+interface ApplicationToolbarParams {
+    title: string | ReactNode;
+    children?: string | ReactNode | ReactNode[];
+    backRoute?: string;
+}
+declare function ApplicationToolbar({ title, children, backRoute, ...rest }: ApplicationToolbarParams): React__default.JSX.Element;
+
+declare function FlexClear(): React__default.JSX.Element;
+
+export { ApplicationToolbar, type ChangeListener, CheckboxSelection, type CheckboxSelectionOption, type CheckboxSelectionProps, type ColumnDefinition, type Configuration, ConfigurationContext, ConfigurationProvider, DataTable, type DataTableProps, type EditorField, type EditorFieldType, FetchError, FlexClear, Form, FormSkeleton, type Icons, type LastQueryInfo, type LazyUseQuery, LinkButton, type LinkButtonProps, type MutationQuery, type MutationQueryResult, type MutationQueryTrigger, Notification, NotificationContext, ObjectEditor, type ObjectEditorRef, type QueryTrigger, type UseQuery, type UseQueryDefinition, type UseQueryResult, buildIconsFromIconSet, useConfiguration, useNotification };
